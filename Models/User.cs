2e36c923ba136c89184043c84544907254ab772a
Kei-K23 <keiksl2301@gmail.com>
2024-09-07 09:40:30 +0630

    Update User model

--- Models/User.cs
+++ Models/User.cs
@@ -7,20 +7,16 @@ namespace LibraryManagementSystemBackend.Models
     {
         [Key]
         public Guid Id { get; set; }
-
         [StringLength(100, MinimumLength = 3)]
         [Required]
         public required string UserName { get; set; }
-
         [StringLength(80, MinimumLength = 3)]
         [Required]
         [EmailAddress]
         public required string Email { get; set; }
-
         [StringLength(255, MinimumLength = 8)]
         [Required]
         public required string Password { get; set; }
-
         public string? ProfilePicture { get; set; }
         public bool? IsBanned { get; set; } = false;
         public bool? IsLocked { get; set; } = false;
